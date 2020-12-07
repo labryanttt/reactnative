3791ba62c7c6c543e25588126646befd9dcd544b
Bryant Albert <labryanttt@yahoo.com>
2020-12-06 21:25:08 -0800

    wk 3 - workshop task 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,6 +26,8 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -52,7 +54,9 @@ function RenderCampsite(props) {
                         }
                     ],
                     { cancelable: false }
-                );
+                )
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
